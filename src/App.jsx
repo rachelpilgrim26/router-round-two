29094b9327158c5902d235ecd96c203ac64ed392
rachelpilgrim26 <pilgrimrachel5@gmail.com>
2025-08-06 13:01:39 -0400

    main

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Gallery from "./pages/Gallery.jsx";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/gallery">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/gallery" element={<Gallery />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Gallery.jsx
@@ -0,0 +1,3 @@
+export default function Gallery() {
+  return <h1>Welcome to the Gallery Page</h1>;
+}
